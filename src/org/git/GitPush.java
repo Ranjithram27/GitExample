9f2c9ac194d2928da50320d0adc2915de664f790
Ranjithram27 <ranjithram327p@gmail.com>
2022-01-10 09:59:43 +0530

    Done

---
+++ src/org/git/GitPush.java
@@ -0,0 +1,17 @@
+package org.git;
+
+public class GitPush {
+	
+	private void example() {
+		System.out.println("Hello");
+
+	}
+	
+	public static void main(String[] args) {
+		
+		GitPush g = new GitPush();
+		
+		g.example();
+	}
+
+}
